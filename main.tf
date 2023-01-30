ba423ae386f8cdb96ec642ac2c47d8d7073c9092
Steven Yampolsky <yampy@Stevens-Mac-Studio.local>
2023-01-30 11:48:00 -0800

    added service accounts

--- main.tf
+++ main.tf
@@ -7,3 +7,15 @@ resource "google_project" "covid_prod" {
   name = "yampy-covid-prod"
   project_id = "yampy-covid-prod"
 }
+
+resource "google_service_account" "covid_dev_sa" {
+  account_id   = "covid-dev-sa"
+  display_name = "Service Account for yampy-covid-dev GCP project"
+  project = google_project.covid_dev.name
+}
+
+resource "google_service_account" "covid_prod_sa" {
+  account_id   = "covid-prod-sa"
+  display_name = "Service Account for yampy-covid-prod GCP project"
+  project = google_project.covid_prod.name
+}
